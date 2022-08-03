9faf629004b589bad837ab08d8ee6dce3ad2222c
Jina Wilde <jina.wilde@my.utsa.edu>
2022-08-02 19:23:13 -0500

    Created deposits and withdrawals
    A journal of every transaction for the bank account class.

--- MySuperBank/Program.cs
+++ MySuperBank/Program.cs
@@ -6,8 +6,8 @@ namespace MySuperBank
     {
         static void Main(string[] args)
         {
-            var account = new BankAccount("Kendra", 1000);
-            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
+            var account = new BankAccount("Kendra", 10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with ${account.Balance}.");
         }
     }
 }
