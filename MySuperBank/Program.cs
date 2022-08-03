814ea8085c207a19cd083017971f30df6f25cfc4
Jina Wilde <jina.wilde@my.utsa.edu>
2022-08-02 19:55:57 -0500

    Log all transactions
    Display a log of all bank transactions

--- MySuperBank/Program.cs
+++ MySuperBank/Program.cs
@@ -11,8 +11,46 @@ namespace MySuperBank
 
             account.MakeWithdrawal(120, DateTime.Now, "Hammock");
             Console.WriteLine($"Your balance is now ${account.Balance}.");
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine($"Your balance is now ${account.Balance}.");
+            account.MakeDeposit(100, DateTime.Now, "Refund");
+            Console.WriteLine($"Your balance is now ${account.Balance}.");
+
+            Console.WriteLine(account.GetAccountHistory());
+
+
+
+
+
+            // Test for a negative balance.
+            try
+            {
+                account.MakeWithdrawal(750000, DateTime.Now, "Attempt to overdraw");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            }
+
+            // Test that the initial balances must be positive.
+            //BankAccount invalidAccount;
+            try
+            {
+                var invalidAccount = new BankAccount("invalid", -55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+                // return;
+            }
+
+            account.MakeWithdrawal(50, DateTime.Now, "Groceries");
+            Console.WriteLine($"Your balance is now ${account.Balance}.");
 
         }
+
     }
 }
 
